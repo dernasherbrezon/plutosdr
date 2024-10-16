390baef9400544fa35bf0f8ab866d254f308083e
dernasherbrezon <dernasherbrezon@gmail.com>
2024-10-16 19:36:46 +0100

    remove unused

--- src/pluto_util.h
+++ src/pluto_util.h
@@ -32,30 +32,10 @@
   } while (0)
 
 typedef enum {
-  RX, TX
-} iio_direction;
-
-typedef enum {
-  IIO_GAIN_MODE_MANUAL = 0,
-  IIO_GAIN_MODE_FAST_ATTACK = 1,
-  IIO_GAIN_MODE_SLOW_ATTACK = 2,
-  IIO_GAIN_MODE_HYBRID = 3
-} gain_mode;
-
-
-typedef enum {
   FORMAT_UNKNOWN = 0,
   FORMAT_CU8 = 1,
   FORMAT_CS16 = 2
 } iq_format;
-
-typedef struct {
-  uint64_t sampling_freq; // Baseband sample rate in Hz
-  uint64_t center_freq; // Local oscillator frequency in Hz
-  gain_mode gain_control_mode;
-  int64_t offset;
-  double manual_gain;
-} channel_config;
 
 struct iio_context * plutosdr_find_first_ctx();
 
